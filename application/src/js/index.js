060f52b4c6039c29aa7233c220fe7f8a41b9926a
bonesbroken <145737816+bonesbroken@users.noreply.github.com>
2025-10-11 01:31:57 -0400

    1.0.1
    render gradient if there's no settings

--- application/src/js/index.js
+++ application/src/js/index.js
@@ -3,16 +3,18 @@ import {drawGradient} from './utils.js';
 document.addEventListener('DOMContentLoaded', () => {
     const query = location.search.substr(1);
 
-    if (query) {
-        query.split('&').forEach(part => {
-            const item = part.split('=');
-            if (item[0] === 'settings' && item[1]) {
-                try {
-                    drawGradient(JSON.parse(decodeURIComponent(item[1])));
-                } catch (err) {
-                    console.error('Failed to parse settings from query string', err);
-                }
-            }
-        });
+    if (query && query.includes('settings=')) {
+      query.split('&').forEach(part => {
+          const item = part.split('=');
+          if (item[0] === 'settings' && item[1]) {
+              try {
+                  drawGradient(JSON.parse(decodeURIComponent(item[1])));
+              } catch (err) {
+                  console.error('Failed to parse settings from query string', err);
+              }
+          }
+      });
+    } else {
+      drawGradient();
     }
 });
